7c4d4370d7fedf23e48c65ce062cd2f0dfdf70e6
Adam Cameron <adamdcameron@gmail.com>
2018-06-12 12:52:36 -0500

    remove stuff

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Article, Category
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'category', 'slug')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    # Creates lookup widget for author, rather than dropdown menu
+    # raw_id_fields = ('author',)
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Article(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category')
+    slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.title
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
